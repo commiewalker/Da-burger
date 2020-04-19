635af865dd0f93d7a55ad2f8bd5a397d105340e0
Commie Walker <commie.walker@hotmail.com>
2020-04-19 15:53:45 -0700

    change connection back

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,14 @@
 //set up connection to mysql
 const mysql = require("mysql");
 
-let connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
+let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "1234",
     database: "burger_db"
-  });
-}
-
-
+});
 
 connection.connect(function (err) {
   if (err) {
